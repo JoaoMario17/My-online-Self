1b5a832e62bb2143ef6ce06cb46a64858b746699
Joao Mario <joao.martins@soluti.com.br>
2022-05-10 11:20:18 -0300

    testing

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <h1>ola</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
   </div>
